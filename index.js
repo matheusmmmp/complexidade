303b12ae74a228d6f1b56fdb042a67c7fd7d6797
matheus <matheusmmmp@hotmail.com>
2019-11-26 20:23:05 -0300

    VAI PORRA

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("cabalo");
+function cpu(tarefa) {
+    var tarefa = this.tarefa;
+    var makespam ;
+    function constructor() {
+      for (var i = 0; i < count; i++) {
+        this[makespam[i]] = this.tarefa[i];
+      }
+    }
+    return constructor;
+  }
+  
+  var machine = cpu();
+  var row = new machine(1, 'john', 'au');
+  console.log(); // displays: john
